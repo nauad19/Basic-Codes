6d3803d2114f9ffe677a2f8c9b87d9fc5317038f
Aditya <adityanaudiyal82@gmail.com>
2025-11-13 21:24:02 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,9 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("GCD =", gcd(x, y))
